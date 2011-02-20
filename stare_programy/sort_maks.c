5a5e895f84b2033f654611c5952a1ed0921cab55
Mateusz <mzarkowski@sigma.inf.ug.edu.pl>
2011-02-20 14:47:44 +0100

    dodalem stare programy

---
+++ stare_programy/sort_maks.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+//sortowanie przez wstawianie
+
+main() {
+
+int i, j, n, x;
+printf("Ilosc liczb: ");
+scanf("%i", &n);
+int t[n];
+  for (i=0; i<n; i++){
+	    scanf("%i", &t[i]);
+	    j=i;
+	    while (j>0 && t[j]<t[j-1]){
+			      x=t[j];
+			      t[j]=t[j-1];
+			      t[j-1]=x;
+			      j--;
+			    }
+	  }
+
+ for (i=0; i<n; i++){
+  printf("%i ", t[i]);
+  }
+printf("\n");
+}
+
