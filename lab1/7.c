37bee878eeeb04ee5661d7843d993d8c7e7e6f61
Mateusz <mzarkowski@sigma.inf.ug.edu.pl>
2011-02-21 19:01:15 +0100

    Zadanie 7 również kompilować przez c++

---
+++ lab1/7.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+
+	printf("Podaj liczbę: ");
+	float b, pierwiastek;
+	scanf("%f", &b);
+
+	if (b == 0) {
+		printf("0 nie jest odwracalne.\n", b);
+
+	}else {
+		printf("Liczba odwrotna do liczby %f to %f\n", b, 1 / b);
+	}
+
+
+	if (b < 0) {
+		printf("Podana liczba nie posiada pierwiastka rzeczywistego");
+	}
+	else{
+		pierwiastek = sqrt(b);
+		printf("Pierwiastek z liczby %f wynosi %f\n", b, pierwiastek);
+	}
+	return 0;
+}
+
+
