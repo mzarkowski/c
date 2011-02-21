82aacbc97443331a4de95fdf37b085304ec9c6af
Mateusz <mzarkowski@sigma.inf.ug.edu.pl>
2011-02-21 17:32:15 +0100

    zadanie 5 jest w dwoch plikach

---
+++ lab1/5a.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+   double deg, rad;
+   printf("Podaj kat w stopniach : ");
+   scanf("%lf", &deg);
+   rad = 2 * M_PI *deg /360 ; /* grep PI /usr/include/math.h */
+   printf("%lf deg = %lf rad\n", deg , rad);
+   return 0;
+}
---
+++ lab1/5b.c
@@ -0,0 +1,21 @@
+/* Program ten dla dwóch liczb wczytanych z klawiatury
+ * wypisuje, która z nich jest większa. */
+#include <stdio.h>
+int main()
+{
+   int licz1, licz2 ;
+
+   printf("Podaj pierwsza liczbe: ");
+   scanf("%d", &licz1);
+   printf("Podaj druga liczbe: ");
+   scanf("%d", &licz2);
+   if (licz1>licz2){
+	      printf("Liczba %i jest większa od liczy %i.\n",licz1,licz2);
+	    }else if(licz1<licz2){
+			     printf("Liczba %i jest większa od liczy %i.\n",licz2,licz1);
+			   }else{
+				      printf("Liczba %i jest równa liczbie %i.\n",licz1,licz2);
+				    }
+     
+   return 0;
+}
